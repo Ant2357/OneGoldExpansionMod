f69fa4bcf093254fc526643b88e98324b65d12a0
Ant2357 <ryu4979@gmail.com>
2025-06-21 04:32:31 +0900

    feat: init

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+using BepInEx;
+using HarmonyLib;
+
+[BepInPlugin("ant2357.one_gold_expansion_mod", "One Gold Expansion Mod", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        var harmony = new Harmony("ant2357.one_gold_expansion_mod");
+        harmony.PatchAll();
+    }
+}
+
+// 土地拡張費用を常に金塊 1 個にするパッチ
+[HarmonyPatch(typeof(CalcGold), nameof(CalcGold.ExpandLand))]
+public static class ExpandLandPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(ref int __result)
+    {
+        __result = 1;
+    }
+}
